b9bb857f4a561656d328cd536323ef33dc811569
a-siddeley <asiddeley@rogers.com>
2017-10-03 08:05:15 -0400

    pairs tag rules

---
+++ soup/AIengine.js
@@ -0,0 +1,254 @@
+/**********************************
+S iddeley's
+O pen
+U tility
+P ackage
+
+Andrew Siddeley
+20-Sep-2017
+********************************/
+
+window.AIengine=function(options){
+	
+	if (this===window) {return new aiWordMatcher(options);};
+
+	this.createResult=function(options){
+		/***
+		returns a new result object with all default field:vlaues and allows specified fields to be overidden via options object.  Non-jQuery code - Object.create({options});
+		***/
+		options=(typeof options == "undefined")?{}:options;
+		return $.extend({},{
+			matched:false,
+			patternStr:"",
+			range:"",
+			score:0,
+		}, options);	
+	};
+	
+	/////////////////////////////////////////////////////////
+	this.defs={};
+	
+	this.defsFromHTML=function(def$){
+		var that=this;
+		var left$=def$.children("left");
+		var right$=def$.children("right");
+		if (left$.length!=right$.length)
+			{console.log("Error, lefts and rights are not paired in definitions"); return;}
+		else {
+			left$.map(function(index, element){
+			that.define($(element).text(), $(right$[index]).text());			
+		});}
+	};
+	
+	this.define=function(left, right){
+		//eg. ("[noun]", "[person]|[place]|[thing]")
+		//resolve definitions here, not at match time! 
+		//if definition is a function, make sure it returns T/F within 6 recurrsions then store it.
+		//if definition is a string and a function seed, prepare the function and store.
+		//that should cover all cases.
+		var that=this;
+		if (typeof left != "string") {
+			console.log("syntax error - wordDef requires text/string arguments"); 
+			return null;
+		}
+		else if (typeof right == "function") { 
+			this.defs[left]=right;
+			//console.log(this.fn[left]);
+		}
+		else if ((typeof right == "string")&&(right.indexOf("|")!=-1)){
+			//definition contains an operator	
+			//create function that compares range word to right words (rw)
+			var rightWords=right.split("|");
+			this.defs[left]=function(word, index, range){
+				//loops through matches until true (matched), otherwise returns false
+				return rightWords.some(function(rw){return (word==rw);});
+			}
+			//console.log(this.fn[left]);
+		} 
+		else if (typeof right == "string"){	
+			//definition right value is just a string so
+			//create simple function that compares it to the word provided from range 
+			this.defs[left]=function(word, index, range){return(word==right);};
+			//console.log(this.fn[left]);
+		};		
+			
+	};
+	
+	///////////////////////////////////////////////////////////////////
+	this.fn={};
+	this.fn.inList=function(item, list){
+		//list checker
+		//returns true if item is in the list AKA Array
+		return (listHash.indexOf(item)!=-1);
+	};
+	this.fn.inHash=function(item, hash){
+		//hash checker
+		//returns true if item is a key or value in the hash AKA Object
+		return (Object.keys(hash).indexOf(item)!=-1)||(Object.values(hash).indexOf(item)=-1);
+	};
+	
+	/////////////////////////////////////////////////////////////////
+	this.getBestResponse=function(){
+		//find first true match
+
+		var index=-1;
+		var response;
+		if (this.results.some(function(r){index=r[0].index;	return r[0].match;}))
+			{response=this.pairs[index].response;}
+		else {response="I don't understand";}
+		
+		if (typeof response=="function") {return response();}
+		else {return response;}
+	
+	};
+	
+	this.groom=function(str){
+		//grooming returns a clean array of words
+		//console.log("before",str);
+		//ensure good separation by padding various separators and punctuators
+		str=str.replace(/\]/g,"] "); 
+		str=str.replace(/\.\s/g," . "); //ignores [...] and chaining periods such as www.com
+		//str=str.replace(/,/g," , ");
+		//TO DO - ampersand&, comma, exclamation!, question mark?, quote"  
+
+		//split at spaces
+		return str.split(/\s+/gm);
+	};
+	
+	////////////////////////////////////////	
+	this.match=function(inputStr){
+		var that=this;
+		this.matchInputStr=this.groom(inputStr);
+		//[].some stops at first true, needs false to continue
+		this.matchResult=this.pairs.some(function(pair, i){
+			//patternResult=that.matchMap(pair.pattern, this.inputStr, 0);
+			return that.matchMap(pair.pattern);
+		});
+	};
+	
+	this.matchExec=null; //current regex.exec() result as a [] or null if nomatch
+	this.matchInputStr=""; //current input string for matching
+	this.matchTerm=""; //current regex capture group ie. matchExec[1]..[n]
+	this.matchResult=false;
+	
+	this.matchMap=function(pattern){
+		//pattern	{predicates:[fn1, fn2...], regexp:/(term1)(term2)(term3)/}
+		//where		a term may be a wildcard at location of predicate such a list checker
+		//input  	[word1, word2...]
+		//result	[1,1,1,1,0]
+		//return	[{head}, {term result}, {term result}...]
+		//Example 	[{pattnum:1, match:true, score:0.95}, {term:noun, target:cat, match:true}...]
+		var matchResult;		
+		var that=this;
+		this.matchExec=pattern.regexp.exec(this.matchInputStr);
+		//eg. [0]=matching string, [1]..[n]=matched terms, ["index"]=match index in string
+		
+		//Regex Matches
+		if (this.matchExec!=null){
+			//Returns true only if all predicates are true. 
+			//Also returns true if the predicates array if empty which is good
+			matchResult=pattern.predicates.every(function(p,i,a){
+				that.matchTerm=that.matchExec[i+1];
+				return p(that);
+			});	
+		}
+		return matchResult;
+	};
+
+	///////////////////////////////////////////////////
+	this.pairsFromHTML=function(pairs$){
+		var that=this;
+		var patt$=pairs$.children("patt");
+		var resp$=pairs$.children("resp");
+		if (patt$.length!=resp$.length)
+			{console.log("Error, patterns and responses are not paired"); return;}
+		else {
+			patt$.map(function(index, element){
+				that.pattern($(element).text(), $(resp$[index]).text());			
+			});
+		}
+	};
+	
+	this.pattern=function(patternStr, response){
+		//adds a word pattern to wordMatcher
+		//[...] hello [punc] my name is [properNoun arg1][...]
+		//this.pairs.push({pattern:this.groom(patternStr), response:response});
+		this.pairs.push({pattern:this.patternizer(patternStr), response:response});
+	};
+	
+	this.patternizer=function(patternStr){
+		//Parses patternStr and builds predicates[] and Regexp //
+		//patternStr Eg. "$isBotmaster [noun] is * a *"
+		//patternStr profile "$predicate $predicate() is * a *"
+		//patternStr eg. has 6 terms, term 1, 2, 4 & 6 have regex wildcards
+		//For pattern to match, all predicates and regexp must be true. 
+		//returns {predicates:[], regexp:RegExp}
+		var predicates=[];
+		var regexp="";
+		var word="(\\w+\\s+)"; //regex capture group for a word
+		var nada="()"; //regex capture group to act as a predicate placeholder
+		var star="(.*)"; //regex capture group to act as a wildcard for a list tesing predicate
+		var flag="i"; //ignore case, global, multiline
+		var that=this;
+		var profile=patternStr.split(" ").map(function(term, index, arr){
+			if (term.charAt(0)=="$"){
+				//$hello - predicate variable wrapper
+				if (term.charAt(term.length-1)!=")") {
+					predicates.push(new Function("ai", "return ai.defs["+term+"];"));
+					regex+=nada;
+					return "$predicate";
+				}
+				//$hello(arg1,arg2) - predicate function handler
+				else {
+					var fnam=term.substring(0,term.indexOf("(")-1);
+					var args=term.substring(term.indexOf("(")+1, term.indexOf(")")-1);
+					predicates.push(new Function("ai","return ai.defs["+fnam+"].('"+args+"');"));
+					regex+=nada;
+					return "$predicate()";
+				}
+			}
+			else if (term.charAt(0)=="[") {
+				//[list] - list wrapper
+				//eg. term="[nouns]", ai.defs["[nouns]"]=["person","place"]
+				predicates.push(new Function("ai",
+					//inList(item, list); //returns true if item in list
+					"return ai.fn.inList(ai.matchTerm, ai.defs["+term+"]);"
+				));
+				regex+=word;
+				return "[list]";
+			}
+			else if (term.charAt(0)=="{") {
+				//{list} - hash wrapper
+				//eg. term="{nouns}", ai.defs["{nouns}"]={"andrew":"human"}
+				predicates.push(new Function("ai",
+					"return ai.fn.inHash(ai.matchTerm, ai.defs["+term+"]);"
+				));
+				regex+=word;
+				return "[list]";
+			}
+			else if (term.charAt(0)=="*") {regex+=star;	return "*wildcard";}
+			else {regexp+="("+term+"\s)"; return "specific_word";}
+		});
+		console.log("patternizer profile:",JSON.stringify(profile));
+		return {predicates:predicates, regexp:new Regexp(regexp, flag)};
+	};
+	
+	//Pattern Response Storage
+	//["["function(words){return true;}, "hello",...][...][...]]
+	this.pairs=[];
+}; //function
+
+
+// Define a Module with Simplified CommonJS Wrapper...
+// see http://requirejs.org/docs/api.html#cjsmodule
+if (typeof define=="function"){
+	define(function(require, exports, module){ return aiWordMatcher});
+} 
+else { 
+	//console.log("window.aiWordMatcher", aiWordMatcher);
+	//window.aiWordMatcher=aiWordMatcher;
+}
+
+
+
+
