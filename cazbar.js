9fd1b59eb724b282157444297d300d4b75bd6377
Andrew Siddeley <asiddeley@norr.com>
2017-12-07 17:03:34 -0500

    Project Form SQLs

--- cazbar.js
+++ cazbar.js
@@ -7,36 +7,14 @@ const bodyParser=require("body-parser")
 
 var app = express()
 
+//Main entry
+app.get('/', function (req, res) {res.sendFile(path.join(__dirname + "/cazbar.html"));})
 
-app.use( bodyParser.urlencoded({extended: true}));
-app.use( bodyParser.json());
+//Static file server.  Use '../' to get parent path
+app.use(express.static(path.join(__dirname)));
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + "/cazbar.html"))
-
-})
-
-app.post('/formHandler', function (req, res) {
-	//console.log("POST /formHanlder...", req.url) 
-	console.log("POST /formHanlder... req.body:", req.body)
-	
-	let msg="success"
-	var zb=new sqlite.Database("/zbar/cazbar.db")
-	zb.serialize(function () {
-		zb.run(req.body.SQL, req.body, function(err){console.log(err); msg=err })
-	})
-	zb.close()	
-	
-	res.send(msg)
-})
-
-
-//logger
-app.use(function(req, res, next){
-	console.log("LOG...",req.url)
-	next()
-});
-
+//Logger
+app.use(function(req, res, next){console.log("LOG...",req.url);	next();});
 
 /*************************
 bodyParser.urlencoded(options)
@@ -48,34 +26,68 @@ Parses the text as JSON and exposes the resulting object on req.body.
 THANKS TO
 https://stackoverflow.com/questions/4295782/how-do-you-extract-post-data-in-node-js
 *********************/
+app.use( bodyParser.urlencoded({extended: true}));
+app.use( bodyParser.json());
 
+//Database form handler and required middleware (above)
+app.post('/formHandler', function (req, res) {
+	//console.log("POST /formHanlder...", req.url) 
+	console.log("POST /formHanlder... req.body:", req.body)
+	
+	let msg="success"
+	let db=new sqlite.Database("cazbar.db")
+	let sqls=req.body.SQL.split(";")
+	let rows=[]
+	
+	db.serialize(function () {
+		for (var i in sqls){
+			
+			//switch based on first term of current SQL
+			switch(sqls[i].split(" ")[0]){
+				case "CREATE": 
+				db.run(sql_params(sqls[i], req.body), [], function(err){
+					console.log("db.run...", err); msg=err 
+				})
+				case "UPDATE":
+				db.run(sql_params(sqls[i], req.body), [], function(err){
+					console.log("db.run...", err);
+					msg=err 
+				})
+				case "SELECT":
+				db.each(sql_params(sqls[i], req.body), [], function(err, row){
+					console.log("db.each...", err, row);
+					rows.push(row)
+					msg=err 
+				})
+			}
+		}
+	})
+	db.close()	
+	
+	//send results
+	res.send({msg:msg, rows:rows})
+})
 
-//serve static files
-//app.use(express.static(path.join(__dirname, '../'))); //to get parent path
-app.use(express.static(path.join(__dirname)));
 
 //start serving
 app.listen(8080, function () {console.log('CAzbar server listening on port 8080!')});
 
 
-////////////////////////////////////
-//zoombase Cazbar Database
-//
-
-function sqliteExample(){
-
-	var zb=sqlite(':memory:')
-	zb.serialize(function () {
-
-		zb.run('CREATE TABLE ipsum (info TEXT)')
-		var stmt = zb.prepare('INSERT INTO ipsum VALUES (?)')
-		for (var i=0; i<10; i++) {stmt.run('value '+i)}
-		stmt.finalize()
-
-		zb.each('SELECT rowid AS id, info FROM ipsum' , function (err, row) {
-			console.log(row.id + ': ' + row.info)
-		})
-	})
-	zb.close()
+//prepare SQL by substituting parameters with corresponding values
+function sql_params(sql, params){
+	//ensure these are not touching term
+	sql=sql.replace("(", " ( ") 
+	sql=sql.replace(")", " ) ") 
+	sql=sql.replace("=", " = ") 
+	sql=sql.replace(",", " , ") 
+	var terms=sql.split(" ") //break SQL into terms (words) 
+	for (var i in terms){
+		//term starts with '$' meaning it's a parameter, substitute it with it's corresponding vlaue
+		if ( terms[i].indexOf("$")==0 ) {
+			terms[i]=params[terms[i].substring(1)]
+		}		
+	}
+	console.log("SQL...", sql, "...after substitutions...", terms.join(" "))
+	return terms.join(" ") //unsplit
 }
 
