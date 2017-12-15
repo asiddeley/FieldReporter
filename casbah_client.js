67d825ce7cb48c28ded8e5013d10661569653fa1
a-siddeley <asiddeley@rogers.com>
2017-12-15 08:14:21 -0500

    autoForm working

--- casbah_client.js
+++ casbah_client.js
@@ -12,34 +12,62 @@ Andrew Siddeley
 13-Dec-2017
 ********************************/
 
-
-const autoForm(div$, params){
+const autoForm=function(div$, params){
 	//params={table:"projects", rows:[{pnum:"abc", pname:"abc",...},{},{}], ccsclass:[]}
-	var cont$=$("<div class='container-fluid'></div>");	
+	var con$=$("<div class='container-fluid'></div>");	
 	div$.empty();
-	div$.append(cont$);
+	div$.append(con$);
 
 	var labl$=$("<h3 class='label label-default'>"+params.table+"</h3>");
 	var form$=$("<form></form>");
-	cont$.append($labl, $form);
-
+	form$.submit(function(ev){
+		ev.preventDefault();
+		//get values from fields including one with SQL or 
+		//submitHandler()
+	});
+	con$.append(labl$, form$);
+	
+	var row, ig$, name$, inpt$;
 	for (var i in params.rows){
-		autoFormIG(form$, params, i);		
+		row=params.rows[i];
+		ig$=$("<div class='input-group'></div>");
+		for (var j in row){
+			name$=("<span class='input-group-addon'>"+ j +"</span>");
+			inpt$=("<input name='"+ row[j] +"' class='form-control' type='text'  placeholder='...' 		aria-describedby='basic-addon1' value='"+row[j]+"'>");
+			ig$.append(name$, inpt$);
+		}
+		form$.append(ig$);		
 	}
+	
+	var bg1$=$("<div class='btn-group' role='group' aria-label='...'>");
+	var btn1=$("<button class='btn btn-primary'>Add</button>");
+	var btn2=$("<button class='btn btn-default'>Current</button>");
+	var btn3=$("<button class='btn btn-default'>Delete</button>");
+	var btn4=$("<button class='btn btn-default'>Save</button>");
+	var btn5=$("<button class='btn btn-primary'>First</button>");
+	var btn6=$("<button class='btn btn-default'>&lt;&lt;&nbsp;Back</button>");
+	var btn7=$("<button class='btn btn-default'>Next&nbsp;&gt;&gt;</button>");
+	var btn8=$("<button class='btn btn-default'>Last</button>");
+	bg1$.append(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8);
+	form$.append(bg1$);
+	
 }
 
-const autoFormIG(form$, params, i){
+function autoFormIG(form$, params, i){
 	//row = {pnum:val, pname:val...  }
-	var row=params.row[i];
+	var row=params.rows[i];
 	var ig$=$("<div class='input-group'></div>");
 	var name$, inpt$; 
 	for (var j in row){
-		name$=("<span class='input-group-addon'>"+ row[j] +"Project number</span>");
-		inpt$=("<input name='"+ row[j] +"' class='form-control' type='text'  placeholder='...' aria-describedby='' val='"+row[j]+"'>");
+		name$=("<span class='input-group-addon'>"+ row[j] +"</span>");
+		inpt$=("<input name='"+ row[j] +"' class='form-control' type='text'  placeholder='...' aria-describedby='basic-addon1' value='"+row[j]+"'>");
 		ig$.append(name$, inpt$);
 	}
 	form$.append(ig$);
 }
+
+
+
 
 
 const ieSplice=function(list, index, remove, ins){
